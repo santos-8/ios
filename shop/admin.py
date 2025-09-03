aba51885e4c17758e4f894183e857c05299bf7a7
Alexander <xanderariole@gmail.com>
2025-09-03 12:37:34 +0100

    Registered some urls at the admin.py file

--- shop/admin.py
+++ shop/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from shop.models import User, Phone, Order
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Phone)
+admin.site.register(Order)
